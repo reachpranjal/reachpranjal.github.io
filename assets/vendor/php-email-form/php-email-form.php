473cf9739c94204e6feb7f127d909ebd25d42ef1
Pranjal <65755220+reachpranjal@users.noreply.github.com>
2020-09-01 11:42:56 +0530

    Update php-email-form.php

--- assets/vendor/php-email-form/php-email-form.php
+++ assets/vendor/php-email-form/php-email-form.php
@@ -127,10 +127,10 @@ class PHP_Email_Form {
         $this->error .= 'SMTP password is empty!' . '<br>';
     
       if( !isset( $this->smtp['port'] ) )
-        $this->smtp['port'] = 587;
+        $this->smtp['port'] = 465;
       
       if( !isset( $this->smtp['encryption'] ) )
-        $this->smtp['encryption'] = 'tls';
+        $this->smtp['encryption'] = 'ssl';
 
       if( isset( $this->smtp['mailer'] ) ) {
         $this->mailer = $this->smtp['mailer'];
